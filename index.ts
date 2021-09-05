7b92c5bb9ad11e23dfbc51acf559f475a27d9145
tung <>
2021-09-05 20:40:28 +0700

    add cors

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import connection from './db/db.init';
 import mainRouter from './src/routers'
 import bodyParser from 'body-parser';
+import cors from 'cors'
 const app = express();
 
 connection.then(res => {
@@ -9,7 +10,7 @@ connection.then(res => {
 }).catch(err => {
     console.log('err : ', err);
 })
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(mainRouter)
